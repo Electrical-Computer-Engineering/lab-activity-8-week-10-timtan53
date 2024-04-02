e23fc55c7bf03fa2dd5a43a3377f8ad56d156e60
timtan53 <timmy.tan@stonybrook.edu>
2024-04-02 17:12:00 -0400

    my first submission

--- part1_question1.c
+++ part1_question1.c
@@ -1,1 +1,54 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int my_isalpha(char letter)
+{
+    if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'))
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int my_isdigit(char digit)
+{
+    if (digit >= '0' && digit <= '9')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int my_is_upper(char letter)
+{
+    if (letter >= 'A' && letter <= 'Z')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+char my_to_upper(char letter)
+{
+    if (letter >= 'a' && letter <= 'z')
+    {
+        return toupper(letter);
+    }
+    return letter;
+}
+
+char my_to_lower(char letter)
+{
+    if (letter >= 'A' && letter <= 'Z')
+    {
+        return tolower(letter);
+    }
+    return letter;
+}
+
+int main()
+{
+    char letter1 = 'A', letter2 = 'b', number = '123';
+    printf("Is alphabet %c - %d\nIs digit %c - %d\nIs uppercase %c - %d\nTo Upper %c - %c\nTo Lower %c - %c", letter1, my_isalpha(letter1), number, my_isdigit(number), letter1, my_is_upper(letter1), letter2, my_to_upper(letter2), letter1, my_to_lower(letter1));
+}
 
--- part2_question1.c
+++ part2_question1.c
@@ -1,1 +1,71 @@
+#include <stdio.h>
 
+int my_strcmp(char String1[], char String2[])
+{   
+    int length = 0, i = 0;
+
+    while (String1[i] != '\0')
+    {
+        length++;
+        if (String1[i] == String2[i])
+            return 1;
+        else
+            return 0;
+        i++;
+    }
+}
+
+int my_strncmp(char string1[], char string2[], int n)
+{
+    for (int k = 0; k < n; k++)
+    {
+        if (string1[k] != string2[k])
+            return 0;
+    }
+    return 1;
+}
+
+char *my_strcpy(char string1[], char string2[])
+{
+    int i = 0;
+    while (string2[i] != '\0')
+    {
+        string1[i] = string2[i];
+        i++;
+    }
+    return string1;
+}
+
+char *my_strcat(char string1[], char string2[])
+{
+    int i = 0, k= 0;
+    while (string1[k] != '\0')
+    {
+        k++;
+    }
+    while (string2[i] != '\0')
+    {
+        string1[k+i] = string2[i];
+        i++;
+    }
+    return string1;
+}
+
+char *my_reverse(char string1[], char string2[])
+{
+    int i = 0, j = 0;
+    while (string2[i] != '\0')
+    {
+        i++;
+    }
+    return string1;
+}
+
+int main()
+{
+    char one[32] = "___________" , two[32] = "dude";
+    printf("reverse = %s\n", my_reverse(one, two));
+    // printf("compare first 2 = %d\n", my_strncmp(one, two, 2));
+    // printf("append = %s\n", my_strcat(one, two));
+    // printf("copy = %s\n", my_strcpy(one, two));
+}
